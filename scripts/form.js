841486578fbc2777af0e07b2e9afac33169a3acb
Oscar Sebastián <osebastianv@gmail.com>
2018-01-23 23:39:25 +0100

    added textarea validation

--- scripts/form.js
+++ scripts/form.js
@@ -1,9 +1,12 @@
+//Obtenemos elemento formulario y sus campos
 var form = document.getElementsByName("contacto")[0];
 
 var nombreInput = document.getElementById("nombre");
 var apellidosInput = document.getElementById("apellidos");
 var emailInput = document.getElementById("email");
 var telefonoInput = document.getElementById("telefono");
+var comentariosInput = document.getElementById("comentarios-texto");
+//console.log(comentariosInput);
 
 //var tipoConocidoRadioInput = document.getElementsByClassName("radio");
 //console.log(tipoConocidoRadioInput);
@@ -14,6 +17,7 @@ var tipoConocidoInput = {
   tipoConocido3: document.getElementById("tipo_conocido_3")
 };
 
+//Obtenemos elemento radio junto a su campo de texto otros
 var contactoTipoConocido = document.getElementById("contacto-tipo-conocido");
 var otros = document.getElementsByName("otros");
 
@@ -21,7 +25,7 @@ var otros = document.getElementsByName("otros");
 /*var containerHtml = '<div class="floating-box"></div>';
 var containerElement = document.createElement("div");*/
 
-var textHtml = '<input type="text" name="otros" maxlength="20">';
+var textHtml = '<input type="text" name="otros" id="otros" maxlength="20">';
 var textElement = document.createElement("div");
 textElement.innerHTML = textHtml;
 
@@ -58,13 +62,20 @@ function enviarInfo(event) {
   }
 
   //Validamos el teléfono
+  var telefono = telefonoInput.value.trim();
+
+  if (telefono.length != 9) {
+    alert("Escriba un teléfono correcto, por favor (9 dígitos)");
+    telefonoInput.focus();
+    event.preventDefault();
+    return false;
+  }
+
   var regex = /[6|9][0-9]+/;
-  var resultPhoneValidation = regex.test(telefonoInput.value);
+  var resultPhoneValidation = regex.test(telefono);
 
   if (resultPhoneValidation === false) {
-    alert(
-      "Escriba un teléfono correcto, por favor (9 caracteres, que empiecen por 6 o 9)"
-    );
+    alert("Escriba un teléfono correcto, por favor (debe empezar por 6 o 9)");
     telefonoInput.focus();
     event.preventDefault();
     return false;
@@ -81,6 +92,7 @@ function enviarInfo(event) {
   if (currentIndex == 2) {
     if (otros[0].value.trim() == "") {
       alert("Escriba la opción de cómo me has conocido, por favor");
+      otros[0].focus();
       event.preventDefault();
       return false;
     }
@@ -102,7 +114,7 @@ var radioInput = document.querySelectorAll("input[type=radio]");
 
 for (var i = 0; i < radioInput.length; i++) {
   radioInput[i].addEventListener("click", cambiarOpcionConocido, 0);
-  console.log("L " + i);
+  //console.log("L " + i);
 }
 
 var currentIndex = -1;
@@ -130,7 +142,8 @@ function cambiarOpcionConocido(event) {
     textElement.value = "";
     //contactoTipoConocido.appendChild(textElement);
     contactoTipoConocido.appendChild(textElement);
-    textElement.focus();
+    //textElement.focus();
+    otros[0].focus();
   } else {
     if (currentIndex == 2) {
       //Borro campo de texto otros
@@ -140,33 +153,81 @@ function cambiarOpcionConocido(event) {
   }
 
   currentIndex = newIndex;
+}
 
-  //  alert(this);
+//Contar palabras de textarea
+comentariosInput.addEventListener("keypress", filtrarIntro);
 
-  /*var radioInput = document.querySelectorAll("input[type=radio]"),
-  //  x = radioInput.length;
-  //while (x--) radioInput[x].addEventListener("change", cambiarOpcionConocido, 0);
-  for(var i = 0; i < radioInput.length; i++){
-      radioInput[i].addEventListener("change", cambiarOpcionConocido, 0);
+function filtrarIntro(event) {
+  //Filtramos 13. Intro Y 17. Ctrl. Este último para evitar copy paste
+  if (event.keyCode == 13 || event.keyCode == 17) {
+    //this.value = previewText;
+    event.preventDefault();
+    return false;
   }
 
-  function cambiarOpcionConocido(event) {
-    var index = 0;
+  //Para evitar que se escriba el carácter y no entre en keyup
+  if (previewContador >= 150 && event.keyCode == 32) {
+    event.preventDefault();
+    return false;
+  }
+}
 
-    for(var i = 0; i < radioInput.length; i++){
-      if (radioInput[i].checked){
-        index = i;
-        break;
-      }
+comentariosInput.addEventListener("keyup", contarPalabras);
+var previewContador = 0;
+var previewText = "";
 
-      alert (index);
+function contarPalabras(event) {
+  console.log(event.keyCode);
+
+  var comentario = this.value; // + String.fromCharCode(event.keyCode);
+  var arrayPalabras = comentario.split(" ");
+  /*if (arrayPalabras == null) {
+    event.preventDefault();
+    return false;
   }*/
 
-  /*if (tipoConocidoRadioInput[0].checked === true) {
-    alert("1");
-  } else if (tipoConocidoRadioInput[1].checked === true) {
-    alert("2");
-  } else if (tipoConocidoRadioInput[3].checked === true) {
-    alert("3");
+  var contador = 0;
+
+  for (i = 0; i < arrayPalabras.length; i++) {
+    if (arrayPalabras[i].length >= 1) {
+      contador += 1;
+    }
+  }
+
+  console.log(contador.toString());
+
+  previewContador = contador;
+
+  if (contador > 150) {
+    //this.value = previewText;
+    alert(
+      "El contenido escrito sobrepasa las 150 palabras, por lo que no está permitido. Disculpe las molestias."
+    );
+    this.value = previewText;
+    this.focus();
+    event.preventDefault();
+    return false;
+  }
+
+  previewText = this.value;
+
+  /*console.log(event.key);
+  event.preventDefault();
+  return false;*/
+
+  /*if (this.value == "") {
+    console.log("nada");
+  } else {
+    console.log(this.value);
   }*/
+}
+
+//Filtramos paste de textarea
+comentariosInput.addEventListener("paste", filtrarPaste);
+
+//Si queremos ser estrictos desactivamos el paste y listo
+function filtrarPaste(event) {
+  event.preventDefault();
+  return false;
 }
